7a06843feb9b4e8550f161a119d4daab80558061
Marvin Willms <marvin.willms@code.berlin>
2022-04-09 12:07:21 +0200

    Add `JUMPHOST_PRIVATE_KEY` env var

--- tests/switch_jumphost.sh
+++ tests/switch_jumphost.sh
@@ -11,6 +11,7 @@ export JUMPHOST=localhost
 export JUMPHOST_PORT=2222
 export JUMPHOST_USER=tunnel
 export JUMPHOST_LOCALPORT=15432
+export JUMPHOST_PRIVATE_KEY=tests/keys/private/id_rsa
 
 eval "$(ssh-agent -s)"
 cat tests/keys/private/id_rsa | ssh-add -
